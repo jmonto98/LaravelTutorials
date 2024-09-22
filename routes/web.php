e1c42d701ced3ed5cf97f42bf44d7e24f4f91bb7
jmontoya <jmontoya@celerik.com>
2024-09-22 18:04:24 -0500

    Login and Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AdminAuthMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,13 +11,20 @@ Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.ab
 Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name('home.contact');
 
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
-Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
-Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
-Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+
+Route::middleware([AdminAuthMiddleware::class])->group(function () {
+    Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
+    Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
+    Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+});
+
 Route::get('/cart', 'App\Http\Controllers\CartController@index')->name('cart.index');
 Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name('cart.add');
 Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name('cart.removeAll');
+
 Route::get('/image', 'App\Http\Controllers\ImageController@index')->name('image.index');
 Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name('image.save');
 Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name('imagenotdi.index');
 Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name('imagenotdi.save');
+
+Auth::routes();
