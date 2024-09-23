97c8102b4e27867c3adcfee7b1039c238bdf9279
jmontoya <jmontoya@celerik.com>
2024-09-22 19:11:21 -0500

    Microservice Implemented

---
+++ app/Http/Controllers/Api/ProductApiControllerV3.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductCollection;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ProductApiControllerV3 extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $products = new ProductCollection(Product::all());
+        return response()->json($products, 200);
+    }
+
+    public function paginate(): JsonResponse
+    {
+        $products = new ProductCollection(Product::paginate(5));
+        return response()->json($products, 200);
+    }
+
+    public function save(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric|gt:0',
+        ]);
+
+        $product = Product::create($request->only(['name', 'price']));
+
+        return response()->json($product, 200);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AdminAuthMiddleware;
+use Illuminate\Support\Facades\Auth;
+
 
 Route::get('/', function () {
     return view('welcome');
